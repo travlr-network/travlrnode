6f1d00e788e5d4e6632f7dbd9e96ad34cc6675c2
Dipankar Sarkar <me@dipankar.name>
2024-10-18 14:16:29 +0100

    Updated the setup to handls VCs as well

---
+++ src/data/DataStore.ts
@@ -0,0 +1,26 @@
+import { DataStorePlugin } from './DataStorePlugin';
+
+export class DataStore {
+  private dataStore: DataStorePlugin;
+
+  constructor(dataStore: DataStorePlugin) {
+    this.dataStore = dataStore;
+  }
+
+  async storeDataset(datasetId: string, data: any): Promise<void> {
+    await this.dataStore.setData(`dataset:${datasetId}`, data);
+  }
+
+  async getDataset(datasetId: string): Promise<any> {
+    return await this.dataStore.getData(`dataset:${datasetId}`);
+  }
+
+  async listDatasets(): Promise<string[]> {
+    const allKeys = await this.dataStore.getAllKeys();
+    return allKeys.filter(key => key.startsWith('dataset:'));
+  }
+
+  async deleteDataset(datasetId: string): Promise<void> {
+    await this.dataStore.deleteData(`dataset:${datasetId}`);
+  }
+}
